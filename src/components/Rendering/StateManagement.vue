1e40921efbcfe3b59ff5645f9bb89f0d93538b93
fats4 <muhammadfatu.2@gmail.com>
2024-04-19 14:38:41 +0700

    edit

--- src/components/Rendering/StateManagement.vue
+++ src/components/Rendering/StateManagement.vue
@@ -10,9 +10,9 @@ const store = useCounterStore();
         <table class="w-full border border-black">
             <thead>
                 <tr>
-                    <th class="bg-gray-200 px-4 py-2 border border-black">NAME</th>
-                    <th class="bg-gray-200 px-4 py-2 border border-black">QUANTITY</th>
-                    <th class="bg-gray-200 px-4 py-2 border border-black">ACTION</th>
+                    <th class="bg-gray-400 px-4 py-2 border border-black">NAME</th>
+                    <th class="bg-gray-400 px-4 py-2 border border-black">QUANTITY</th>
+                    <th class="bg-gray-400 px-4 py-2 border border-black">ACTION</th>
                 </tr>
             </thead>
             <tbody>
